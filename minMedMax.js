d1c22571e51a8872c69d2521ccdcf573020fd36f
NewBww <bowonwit.a@gmail.com>
2022-08-30 23:00:24 +0700

    feat:finished

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,26 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
+    let numArray = [n1,n2,n3], temp;
+    if (numArray[0] > numArray[1]) {
+        temp = numArray[1];
+        numArray[1] = numArray[0];
+        numArray[0] = temp;
+    }
+    if (numArray[1] > numArray[2]) {
+        temp = numArray[2];
+        numArray[2] = numArray[1];
+        numArray[1] = temp;
+    }
+    if (numArray[0] > numArray[1]) {
+        temp = numArray[1];
+        numArray[1] = numArray[0];
+        numArray[0] = temp;
+    }
+    return {
+        min: numArray[0],
+        mid: numArray[0] === numArray[1] ? numArray[2] : numArray[1],
+        max: numArray[2]
+    };
 }
 module.exports = minMedMax
